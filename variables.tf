3526fdc39f985561c56274387d24c3d1c9ff2925
TimDavis3008 <45167689+TimDavis3008@users.noreply.github.com>
2024-12-30 20:28:01 +0000

    feat: changed variable to be optional and corrected indentation
    Co-authored-by: Jamie <32368259+jamie-stinson@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "talos" {
       primary_dns_server   = string
       secondary_dns_server = string
       ntp_endpoint         = string
-      node_prefix = string
+      node_prefix          = optional(string, "")
       control_plane = object({
         cpu   = string
         memory = string
