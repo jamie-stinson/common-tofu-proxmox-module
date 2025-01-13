2deae95902c310d9adc3659068eae8bd9a7d09cf
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2025-01-13 20:52:22 +0000

    feat: refactor

--- factory.tf
+++ factory.tf
@@ -5,14 +5,12 @@ data "http" "schematic_id" {
 }
 
 resource "proxmox_virtual_environment_download_file" "this" {
-  node_name               = var.talos.node_data.node_name
-  content_type            = "iso"
-  datastore_id            = "local"
-  decompression_algorithm = "gz"
-  overwrite               = false
-
-  url       = "${var.talos.factory.url}/image/${jsondecode(data.http.schematic_id.response_body)["id"]}/${var.talos.cluster.talos_version}/${var.talos.factory.platform}-${var.talos.factory.arch}.raw.gz"
-  file_name = "talos-${jsondecode(data.http.schematic_id.response_body)["id"]}-${var.talos.cluster.talos_version}-${var.talos.factory.platform}-${var.talos.factory.arch}.img"
+  node_name    = var.proxmox.node_name
+  content_type = "iso"
+  datastore_id = "local"
+  overwrite    = false
+  url          = "${var.talos.factory.url}/image/${jsondecode(data.http.schematic_id.response_body)["id"]}/${var.talos.cluster.talos_version}/${var.talos.factory.platform}-${var.talos.factory.arch}.iso"
+  file_name    = "talos-${jsondecode(data.http.schematic_id.response_body)["id"]}-${var.talos.cluster.talos_version}-${var.talos.factory.platform}-${var.talos.factory.arch}.iso"
   lifecycle {
     create_before_destroy = true
   }
--- vms.tf
+++ vms.tf
@@ -1,5 +1,11 @@
 resource "random_string" "this" {
-  for_each = { for key, value in merge(var.talos.node_data.control_plane.nodes, var.talos.node_data.worker.nodes) : key => value }
+  for_each = {
+    for ip in concat(
+      var.talos.cluster.compute.control_plane.nodes,
+      var.talos.cluster.compute.worker.nodes
+    ) : ip => ip
+  }
+
   length   = 8
   lower    = true
   numeric  = true
@@ -8,26 +14,33 @@ resource "random_string" "this" {
 }
 
 resource "random_integer" "this" {
-  for_each    = { for key, value in merge(var.talos.node_data.control_plane.nodes, var.talos.node_data.worker.nodes) : key => value }
+  for_each = {
+    for ip in concat(
+      var.talos.cluster.compute.control_plane.nodes,
+      var.talos.cluster.compute.worker.nodes
+    ) : ip => ip
+  }
+
   min = 1
   max = 50000
 }
 
 resource "proxmox_virtual_environment_vm" "this" {
-  for_each = { for key, value in merge(var.talos.node_data.control_plane.nodes, var.talos.node_data.worker.nodes) : 
-    key => merge(
-      value, 
-      {
-        type     = contains(keys(var.talos.node_data.control_plane.nodes), key) ? "controlplane" : "worker"
-        cpu      = contains(keys(var.talos.node_data.control_plane.nodes), key) ? var.talos.node_data.control_plane.cpu : var.talos.node_data.worker.cpu
-        memory   = contains(keys(var.talos.node_data.control_plane.nodes), key) ? var.talos.node_data.control_plane.memory : var.talos.node_data.worker.memory
-      }
-    )
+  for_each = {
+    for ip in concat(
+      var.talos.cluster.compute.control_plane.nodes,
+      var.talos.cluster.compute.worker.nodes
+    ) : ip => {
+      type        = contains(var.talos.cluster.compute.control_plane.nodes, ip) ? "controlplane" : "worker"
+      subnet_mask = contains(var.talos.cluster.compute.control_plane.nodes, ip) ? var.talos.cluster.compute.control_plane.subnet_mask : var.talos.cluster.compute.worker.subnet_mask
+      cpu         = contains(var.talos.cluster.compute.control_plane.nodes, ip) ? var.talos.cluster.compute.control_plane.cpu : var.talos.cluster.compute.worker.cpu
+      memory      = contains(var.talos.cluster.compute.control_plane.nodes, ip) ? var.talos.cluster.compute.control_plane.memory : var.talos.cluster.compute.worker.memory
+    }
   }
 
-  name      = format("%s%s-%s", var.talos.node_data.node_prefix ,each.value.type, random_string.this[each.key].result)
-  node_name = "${var.talos.node_data.node_name}"
-  tags      = ["terraform", "talos"]
+  name      = format("%s%s-%s", var.proxmox.node_prefix, each.value.type, random_string.this[each.key].result)
+  node_name = var.proxmox.node_name
+  tags      = ["terraform", "talos", "${each.value.type}"]
   vm_id     = random_integer.this[each.key].result
 
   agent {
@@ -39,38 +52,41 @@ resource "proxmox_virtual_environment_vm" "this" {
 
   startup {
     order      = "1"
-    up_delay   = "60"
-    down_delay = "60"
+    up_delay   = "30"
+    down_delay = "30"
   }
 
   cpu {
     cores = tonumber(each.value.cpu)
-    type  = "x86-64-v2"
+    type  = "x86-64-v2-AES"
   }
 
   memory {
     dedicated = tonumber(each.value.memory)
   }
 
+  operating_system {
+      type = "l26"
+  }
 
   disk {
     datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.this.id
     interface    = "virtio0"
-    size         = "${var.talos.node_data.disk_size}"
+    iothread     = true
+    discard      = "on"
+    file_id      = proxmox_virtual_environment_download_file.this.id
+    file_format  = "raw"
+    size         = var.talos.cluster.storage.install_disk_size
   }
 
   initialization {
     dns {
-      servers = [
-        var.talos.node_data.primary_dns_server,
-        var.talos.node_data.secondary_dns_server
-      ]
+      servers = var.talos.cluster.networking.dns_servers
     }
     ip_config {
       ipv4 {
-        address = "${each.key}/24"
-        gateway = "${var.talos.node_data.default_gateway}"
+        address = "${each.key}/${each.value.subnet_mask}"
+        gateway = var.talos.cluster.networking.default_gateway
       }
     }
   }
@@ -78,6 +94,7 @@ resource "proxmox_virtual_environment_vm" "this" {
   network_device {
     bridge = "vmbr0"
   }
+
   depends_on = [
     proxmox_virtual_environment_download_file.this
   ]
