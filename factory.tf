b22b99d59d8f695004af6813988987b8b4917cab
Jamie <32368259+jamie-stinson@users.noreply.github.com>
2025-01-17 20:30:18 +0000

    feat: add support for multiple proxmox nodes (#12)

--- factory.tf
+++ factory.tf
@@ -5,15 +5,18 @@ data "http" "schematic_id" {
 }
 
 resource "proxmox_virtual_environment_download_file" "this" {
-  node_name    = var.proxmox.node_name
-  content_type = "iso"
-  datastore_id = "local"
-  overwrite    = false
-  url          = "${var.talos.factory.url}/image/${jsondecode(data.http.schematic_id.response_body)["id"]}/${var.talos.cluster.talos_version}/${var.talos.factory.platform}-${var.talos.factory.arch}.iso"
-  file_name    = "talos-${jsondecode(data.http.schematic_id.response_body)["id"]}-${var.talos.cluster.talos_version}-${var.talos.factory.platform}-${var.talos.factory.arch}.iso"
+  for_each      = toset(var.proxmox.nodes)
+  node_name     = each.value
+  content_type  = "iso"
+  datastore_id  = "local"
+  overwrite     = false
+  url           = "${var.talos.factory.url}/image/${jsondecode(data.http.schematic_id.response_body)["id"]}/${var.talos.cluster.talos_version}/${var.talos.factory.platform}-${var.talos.factory.arch}.iso"
+  file_name     = "talos-${jsondecode(data.http.schematic_id.response_body)["id"]}-${var.talos.cluster.talos_version}-${var.talos.factory.platform}-${var.talos.factory.arch}.iso"
+  
   lifecycle {
     create_before_destroy = true
   }
+
   depends_on = [
     data.http.schematic_id
   ]
